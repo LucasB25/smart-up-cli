d700be4208d7ccc0fc73fa830b45152f2d49cc95
LucasB25 <50886682+LucasB25@users.noreply.github.com>
2025-12-03 10:13:26 +0100

    1.1.2

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import path from 'path';
 const program = new Command();
 
 program
-  .version('1.1.0')
+  .version('1.1.2')
   .description('A smart and interactive NPM updater')
   .action(async () => {
     await runSmartUpdate();
